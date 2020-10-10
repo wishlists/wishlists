bf15900c03e5971f4b3153371d436e164be92dea
xiafanzeng <xiafanzeng@nyu.edu>
2020-10-10 14:54:28 -0400

    Created test_service.py and test_model.py

---
+++ test_models.py
@@ -0,0 +1,39 @@
+"""
+Test cases for Wishlist Model
+"""
+import logging
+import unittest
+import os
+from service.models import YourResourceModel, DataValidationError, db
+
+######################################################################
+#  W I S H L I S T   M O D E L   T E S T   C A S E S
+######################################################################
+class TestWishlistModel(unittest.TestCase):
+    """ Test Cases for Wishlist Model """
+
+    @classmethod
+    def setUpClass(cls):
+        """ This runs once before the entire test suite """
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        """ This runs once after the entire test suite """
+        pass
+
+    def setUp(self):
+        """ This runs before each test """
+        pass
+
+    def tearDown(self):
+        """ This runs after each test """
+        pass
+
+######################################################################
+#  P L A C E   T E S T   C A S E S   H E R E 
+######################################################################
+
+    def test_XXXX(self):
+        """ Test something """
+        self.assertTrue(True)
