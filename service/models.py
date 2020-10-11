651ca285036c15f364201d513c55fe7c6a761957
Yuyang Fan <yf1357@nyu.edu>
2020-10-11 18:03:49 -0400

    add CREATE wishlist API

--- service/models.py
+++ service/models.py
@@ -29,27 +29,33 @@ Attributes:
 import logging
 from enum import Enum
 from flask_sqlalchemy import SQLAlchemy
+import json
+
 
 logger = logging.getLogger("flask.app")
 
+
 # Create the SQLAlchemy object to be initialized later in init_db()
 db = SQLAlchemy()
+
 
 class DataValidationError(Exception):
     """ Used for an data validation errors when deserializing """
     pass
 
-class Wishlist(db.Model):
-    """
-    Class that represents a Wishlist
-    This version uses a relational database for persistence which is hidden
-    from us by SQLAlchemy's object relational mappings (ORM)
-    """
 
-    app = None
+class Item(db.Model):
+    """
+    This class represents an item
+    """
+    ##################################################
     # Table Schema
+    ##################################################
     id = db.Column(db.Integer, primary_key=True)
-    
+    wishlist_id = db.Column(db.Integer, db.ForeignKey('wishlist.id'), nullable=False)
+    product_id = db.Column(db.Integer, nullable=False)
+    product_name = db.Column(db.String(63), nullable=False)
+
     @classmethod
     def init_db(cls, app):
         """ Initializes the database session """
@@ -60,5 +66,135 @@ class Wishlist(db.Model):
         app.app_context().push()
         db.create_all()  # make our sqlalchemy tables
 
+    def __repr__(self):
+        return "<Item %r id=[%s] wishlist_id[%s] product_id[%s]>" % (self.product_name, self.id, self.wishlist_id, self.product_id)
+
+    def __str__(self):
+        return "%s: product_id: %s, item_id: %s, wishlist_id: %s" % (
+            self.product_name, self.product_id, self.id, self.wishlist_id)
+
+    def serialize(self):
+        """ Serializes an Item into a dictionary """
+        return {
+            "id": self.id,
+            "wishlist_id": self.wishlist_id,
+            "product_id": self.product_id,
+            "product_name": self.product_name
+        }
+
+    def deserialize(self, data):
+        """
+        Deserializes an Item from a dictionary
+        Args:
+            data (dict): A dictionary containing the resource data
+        """
+        try:
+            self.wishlist_id = data["wishlist_id"]
+            self.product_id = data["product_id"]
+            self.product_name = data["product_name"]
+        except KeyError as error:
+            raise DataValidationError("Invalid Item: missing " + error.args[0])
+        except TypeError:
+            raise DataValidationError(
+                "Invalid Item: body of request contained" "bad or no data"
+            )
+        return self
+
+
+class Wishlist(db.Model):
+    """
+    Class that represents a Wishlist
+
+    This version uses a relational database for persistence which is hidden
+    from us by SQLAlchemy's object relational mappings (ORM)
+    """
+
+    logger = logging.getLogger(__name__)
+    app = None
+
+    def __repr__(self):
+        return "<Wishlist %r user_id=[%s] items[%s]>" % (self.name, self.user_id, self.items)
+
+    def __str__(self):
+        return "%s: id: %s, user_id: %s, items: %s" % (
+            self.name, self.id, self.user_id, str(self.items))
+
+    ##################################################
+    # Table Schema
+    ##################################################
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(63), nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
+    items = db.relationship('Item', backref='account', lazy=True)
+
+    @classmethod
+    def init_db(cls, app):
+        """ Initializes the database session """
+        logger.info("Initializing database")
+        cls.app = app
+        # This is where we initialize SQLAlchemy from the Flask app
+        db.init_app(app)
+        app.app_context().push()
+        db.create_all()  # make our sqlalchemy tables
+
+    def serialize(self):
+        """ Serializes a Wishlist into a dictionary """
+        wishlist = {
+            "id": self.id,
+            "name": self.name,
+            "user_id": self.user_id,
+            "items": []
+        }
+
+        for item in self.items:
+            wishlist['items'].append(item.serialize())
+
+        return wishlist
+
+    def deserialize(self, data: dict):
+        """
+        Deserializes a Wishlist from a dictionary
+
+        :param data: a dictionary of attributes
+        :type data: dict
+
+        :return: a reference to self
+        :rtype: Wishlist
+
+        """
+        try:
+            self.name = data["name"]
+            self.user_id = data["user_id"]
+            item_list = data.get("items")
+            for json_item in item_list:
+                item = Item()
+                item.deserialize(json_item)
+                self.items.append(item)
+        except KeyError as error:
+            raise DataValidationError("Invalid Wishlist: missing " + error.args[0])
+        except TypeError:
+            raise DataValidationError(
+                "Invalid wishlist: body of request contained bad or no data"
+            )
+        return self
+ 
+    def create(self):
+        """
+        Creates a Wishlist to the data store
+        """
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def find(cls, wishlist_id: int):
+        """Finds a Wishlist by it's ID
+        :param wishlist_id: the id of the Wishlist to find
+        :type wishlist_id: int
+        :return: an instance with the wishlist_id, or None if not found
+        :rtype: Wishlist
+        """
+        cls.logger.info("Processing lookup for id %s ...", wishlist_id)
+        return cls.query.get(wishlist_id)
 
 
--- service/service.py
+++ service/service.py
@@ -39,90 +39,43 @@ from service.models import Wishlist, DataValidationError
 # Import Flask application
 from . import app
 
-######################################################################
-# Error Handlers
-######################################################################
-@app.errorhandler(DataValidationError)
-def request_validation_error(error):
-    """ Handles Value Errors from bad data """
-    return bad_request(error)
-
-
-@app.errorhandler(status.HTTP_400_BAD_REQUEST)
-def bad_request(error):
-    """ Handles bad reuests with 400_BAD_REQUEST """
-    message = str(error)
-    app.logger.warning(message)
-    return (
-        jsonify(
-            status=status.HTTP_400_BAD_REQUEST, error="Bad Request", message=message
-        ),
-        status.HTTP_400_BAD_REQUEST,
-    )
-
-
-@app.errorhandler(status.HTTP_404_NOT_FOUND)
-def not_found(error):
-    """ Handles resources not found with 404_NOT_FOUND """
-    message = str(error)
-    app.logger.warning(message)
-    return (
-        jsonify(status=status.HTTP_404_NOT_FOUND, error="Not Found", message=message),
-        status.HTTP_404_NOT_FOUND,
-    )
-
-
-@app.errorhandler(status.HTTP_405_METHOD_NOT_ALLOWED)
-def method_not_supported(error):
-    """ Handles unsuppoted HTTP methods with 405_METHOD_NOT_SUPPORTED """
-    message = str(error)
-    app.logger.warning(message)
-    return (
-        jsonify(
-            status=status.HTTP_405_METHOD_NOT_ALLOWED,
-            error="Method not Allowed",
-            message=message,
-        ),
-        status.HTTP_405_METHOD_NOT_ALLOWED,
-    )
-
-
-@app.errorhandler(status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
-def mediatype_not_supported(error):
-    """ Handles unsuppoted media requests with 415_UNSUPPORTED_MEDIA_TYPE """
-    message = str(error)
-    app.logger.warning(message)
-    return (
-        jsonify(
-            status=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
-            error="Unsupported media type",
-            message=message,
-        ),
-        status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
-    )
-
-
-@app.errorhandler(status.HTTP_500_INTERNAL_SERVER_ERROR)
-def internal_server_error(error):
-    """ Handles unexpected server error with 500_SERVER_ERROR """
-    message = str(error)
-    app.logger.error(message)
-    return (
-        jsonify(
-            status=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            error="Internal Server Error",
-            message=message,
-        ),
-        status.HTTP_500_INTERNAL_SERVER_ERROR,
-    )
 
 
 ######################################################################
-# GET INDEX
+# RETRIEVE A WISHLIST
 ######################################################################
-@app.route("/wishlists", methods=["GET"])
-def list_wishlists():
-    return 'Wishlist Home Page';
+@app.route("/wishlists/<int:wishlist_id>", methods=["GET"])
+def get_wishlists(wishlist_id):
+    """
+    Retrieve a single Wishlist
+    This endpoint will return a Wishlist based on it's id
+    """
+    app.logger.info("Request for wishlist with id: %s", wishlist_id)
+    wishlist = Wishlist.find(wishlist_id)
+    if not wishlist:
+        raise NotFound("Wishlist with id '{}' was not found.".format(wishlist_id))
+    return make_response(jsonify(wishlist.serialize()), status.HTTP_200_OK)
+
+
+######################################################################
+# ADD A NEW WISHLIST
+######################################################################
+@app.route("/wishlists", methods=["POST"])
+def creat_wishlists():
+    """
+    Creates a Wishlist
+    This endpoint will create a Wishlist based the data in the body that is posted
+    """
+    app.logger.info("Request to create a wishlist")
+    check_content_type("application/json")
+    wishlist = Wishlist()
+    wishlist.deserialize(request.get_json())
+    wishlist.create()
+    message = wishlist.serialize()
+    location_url = url_for("get_wishlists", wishlist_id=wishlist.id, _external=True)
+    return make_response(
+        jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
+    )
 
 
 ######################################################################
@@ -138,5 +91,5 @@ def check_content_type(content_type):
     """ Checks that the media type is correct """
     if request.headers["Content-Type"] == content_type:
         return
-    app.logger.error("Invalid Content-Type: %s", request.headers["Content-Type"])
+    # app.logger.error("Invalid Content-Type: %s", request.headers["Content-Type"])
     abort(415, "Content-Type must be {}".format(content_type))
