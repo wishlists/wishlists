60a350ff7ee20c68f10e562ccee1f6ce50273423
Jesse1204 <Jesse1204@users.noreply.github.com>
2020-10-16 17:01:47 -0400

    Update test_service.py
    update test cases for error 500 and 405

--- tests/test_service.py
+++ tests/test_service.py
@@ -26,6 +26,7 @@ import logging
 import unittest
 from unittest.mock import MagicMock, patch
 from urllib.parse import quote_plus
+from flask import abort
 from flask_api import status  # HTTP Status Codes
 from service.models import db, Wishlist
 from service.service import app, init_db
@@ -123,10 +124,21 @@ class TestWishlistService(unittest.TestCase):
         resp = self.app.get("/wishlists/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
 
-   
-        
-
-
+    def test_500_internal_server_error(self):
+        """ Test 500_INTERNAL_SERVER_ERROR """
+        @app.route('/wishlists/500')
+        def internal_server_error():
+            abort(500)
+        resp = self.app.get('/wishlists/500')
+        self.assertEqual(resp.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
+    def test_405_method_not_allowed(self):
+        """ Test 405_METHOD_NOT_ALLOWED """
+        @app.route('/wishlists/405')
+        def method_not_allowed():
+            abort(405)
+        resp = self.app.get('/wishlists/405')
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
 ######################################################################
 #   M A I N
