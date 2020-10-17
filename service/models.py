c4840c43b85ceed3f329c3b79f20d99f6392614b
Qizhuo Shen <sqz0914@gmail.com>
2020-10-17 15:46:12 -0400

    Update models.py

--- service/models.py
+++ service/models.py
@@ -256,3 +256,4 @@ class Wishlist(db.Model, PersistentBase):
         """
         cls.logger.info("Processing user id query for %s ...", user_id)
         return cls.query.filter(cls.user_id == user_id)
+    
